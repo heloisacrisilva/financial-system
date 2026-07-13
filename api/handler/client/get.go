dbaa42e68294bbcf3899cdd8fc85c439ba801818
heloisacrisilva <helotanquella@gmail.com>
2026-07-13 10:37:18 -0300

    feat: handler client tests

--- api/handler/client/get.go
+++ api/handler/client/get.go
@@ -11,6 +11,8 @@ import (
 	"gorm.io/gorm"
 )
 
+var getClientDbFunc = repository.GetClientRepository
+
 func GetClientByID(ctx *gin.Context) {
 	logger := config.GetLogger()
 
@@ -21,7 +23,9 @@ func GetClientByID(ctx *gin.Context) {
 		return
 	}
 
-	client, err := repository.GetClientRepository(clientID)
+	//TEST
+	client, err := getClientDbFunc(clientID)
+	// client, err := repository.GetClientRepository(clientID)
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Warnf("Client with ID %s not found.", clientID)
